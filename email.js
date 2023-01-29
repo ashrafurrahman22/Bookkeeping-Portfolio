c0e5e7a9c63b502b1c590845ca5ddacebb0d9f39
ASHRAFUR RAHMAN <you@example.com>
2023-01-29 17:14:32 +0600

    contact-updated-with-email

---
+++ email.js
@@ -0,0 +1,24 @@
+var btn = document.getElementById("sendBtn");
+btn.addEventListener('click', function(e){
+  e.preventDefault()
+  console.log("clicked")
+
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
+
+  var body = 'Name: ' +name + '<br/> Email: ' + email + "<br/> Subject:" + " " + subject + '<br/> Message: ' + " " + message; 
+
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "ashrafur745@gmail.com",
+    Password : "9302181F96CA45F334BF9841AFDBCD294CE5",
+    To : 'ashrafurrahman22@gmail.com',
+    From : "ashrafur745@gmail.com",
+    Subject : "Message From Portfolio Website",
+    Body : body
+}).then(
+  message => alert(message)
+);
+})
